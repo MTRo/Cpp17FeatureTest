b26238b0e7efea4c6c477a4eaf35262a78de1667
Shintaro Minamino <mataro777@gmail.com>
2016-09-06 17:50:00 +0900

    Initial commit

---
+++ Cpp17FeatureTest/main.cpp
@@ -0,0 +1,44 @@
+﻿
+#include <experimental/resumable>
+#include <future>
+#include <chrono>
+#include <iostream>
+
+std::future<bool> DoWork()
+{
+	printf("Task creating.\n");
+
+	auto task = std::async([] {
+		printf("Doing something.\n");
+
+		//Do heavy load here.
+		std::this_thread::sleep_for(std::chrono::seconds(1)); // 1秒
+
+		printf("Finish something.\n");
+		return true;
+	});
+
+	printf("Task created.\n");
+
+	return task;
+}
+
+
+std::future<int> WorkWorkWatching()
+{
+	printf("Start.\n");
+
+	auto task_result = __await DoWork();
+
+	printf("Done.\n");
+
+	return task_result ? 1 : 0;
+}
+
+int main()
+{
+	auto res = WorkWorkWatching().get();
+	printf("Result=%d \n", res);
+
+	system("pause");
+}
